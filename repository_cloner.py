bf305665762a0e8b65de8d1ffafec96fb8122cf9
martinjl <martinjloos@mail.de>
2022-02-03 22:50:55 +0100

    updated repositroy cloner. bugfixes.

--- repository_cloner.py
+++ repository_cloner.py
@@ -16,16 +16,28 @@ logger = logging.getLogger('log')
 def clone_repo(url):
     directory_name = build_name_from_url(url)
     local_dir_path = '/tmp/' + directory_name
+    cloning_error = 1
 
     # check if local_dir_path already used
-
     if not os.path.isdir(local_dir_path):
+        test_repo_url(url)
+        # we're accepting master branch or alternatively main branch
+        # optimally this section is user input dependent
         try:
-            test_repo_url(url)
             git.Repo.clone_from(url, local_dir_path, branch='master')
+            cloning_error = 0
         except git.exc.GitCommandError as exception:
+            logger.info('No "master" branch found. Trying "main".')
+        if cloning_error == 1:
+            try:
+                git.Repo.clone_from(url, local_dir_path, branch='main')
+                cloning_error = 0
+            except git.exc.GitCommandError as exception:
+                logger.info('No "main" branch found. At the moment only master and main supported.')
+        if cloning_error == 1:
             logger.error('Cloning the repository failed. Please verify your provided URL ' + url)
             sys.exit()
+
         return local_dir_path
     else:
         logger.error('Downloading repository failed. Please rename or delete the following directory in order to use '
