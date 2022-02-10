1c106f501b378dfae4d9f9fe817c32988a2e1b1c
martinjl <martinjloos@mail.de>
2022-02-10 02:45:59 +0100

    updated datasets analysis

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -2,6 +2,7 @@
 
 import filter_repository_artefacts
 from os import listdir
+import os
 from os.path import isfile, join
 import re
 from rich import print
@@ -10,7 +11,7 @@ from rich.table import Table
 
 import source_code_analysis
 
-reg_ext = re.compile("(.+)?((.csv$)|(.png$)|(.jp(e)?g$))")
+reg_ext = re.compile("(.+)?((.py$)|(.ipynb$)|(.md$))")
 
 dataset_folders = []
 
@@ -26,21 +27,46 @@ dataset_analysis_result = []
 # We use the retrieved directories from the structure analysis
 
 
-# folder with 'data' / 'im' + 'g' in name + .csv or .png or .jpg inside -> add files to candidates list
+# returns a list of files inside directory path incl subdirectories
+def get_all_files_in_dir(dir_path):
+    list_of_files = os.listdir(dir_path)
+    all_files = list()
+    for f in list_of_files:
+        full_path = os.path.join(dir_path, f)
+        if os.path.isdir(full_path):
+            all_files = all_files + get_all_files_in_dir(full_path)
+        else:
+            all_files.append(full_path)
+
+    return all_files
+
+
+# folder with 'data' / 'im' + 'g' in name + file name not matching regex -> add files to candidates list
+# files with data in name + file name not matching regex -> add files to candidates list
 def analyse_datasets():
     dataset_folders.extend(filter_repository_artefacts.get_dataset_folders())
     for candidate in dataset_folders:
         dir_path = candidate[1]
-
-        dir_data = []
-
-        all_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+        all_files = get_all_files_in_dir(dir_path)
         for file in all_files:
-            if bool(re.match(reg_ext, file)):
-                dir_data.append(file)
-        if dir_data:
-            for data in dir_data:
-                dataset_file_candidates.append(data)
+            if '/' in file:
+                file_name = file.split('/')[-1]
+            else:
+                file_name = file
+
+            # adding files inside folder named ?data? or ?im(a)g? excluding ones that match the regex
+            if (file_name not in dataset_file_candidates) and (not bool(re.match(reg_ext, file_name))):
+                dataset_file_candidates.append(file_name)
+
+    # add files who are named ?data?.? and who do not end with .py etc (see above)
+    # get all files with ?data?.? in name from filter repo structure
+    # add them to dataset_file_candidates list if not already in it
+    files_w_data_in_name = filter_repository_artefacts.get_files_name_data()
+
+    for f in files_w_data_in_name:
+        file_name = f[0]
+        if (file_name not in dataset_file_candidates) and (not bool(re.match(reg_ext, file_name))):
+            dataset_file_candidates.append(file_name)
 
 
 def get_dataset_file_candidates():
@@ -61,14 +87,14 @@ def build_dataset_response(verbose):
     number_of_dataset_files_mentioned_sc = 0
     percentage_dataset_files_in_sc = 0
 
-    if not dataset_folders:
-        dataset_found = 'No'
-        print('found no possible dataset folders')
+    if (not dataset_folders) and (not dataset_file_candidates):
+        dataset_folder_found = 'No'
+        print('found no possible datasets')
     else:
-        dataset_found = 'Yes'
+        dataset_folder_found = 'Yes'
         number_of_dataset_folder_candidates = len(dataset_folders)
         if not dataset_file_candidates:
-            print('found possible dataset folders but no relevant content inside')
+            print('Found possible dataset folders but no relevant content inside')
         else:
             number_of_dataset_file_candidates = len(dataset_file_candidates)
             sc_dataset_analysis.extend((source_code_analysis.get_mentioned_dataset_files()))
@@ -76,7 +102,7 @@ def build_dataset_response(verbose):
             percentage_dataset_files_in_sc = calculate_percentage(number_of_dataset_files_mentioned_sc,
                                                                   number_of_dataset_file_candidates)
 
-    dataset_analysis_result.extend((dataset_found, number_of_dataset_folder_candidates,
+    dataset_analysis_result.extend((dataset_folder_found, number_of_dataset_folder_candidates,
                                     number_of_dataset_file_candidates, number_of_dataset_files_mentioned_sc,
                                     percentage_dataset_files_in_sc))
 
@@ -89,7 +115,7 @@ def build_dataset_response(verbose):
     table = Table(show_header=True, header_style="bold dim")
     table.add_column("property", justify="left")
     table.add_column("value", justify="right")
-    table.add_row('dataset folders found?', dataset_found)
+    table.add_row('dataset folders found?', dataset_folder_found)
     table.add_row('# dataset folder candidates', str(number_of_dataset_folder_candidates))
     table.add_row('# dataset file candidates', str(number_of_dataset_file_candidates))
     table.add_row('# found dataset file candidates mentioned in source code', str(number_of_dataset_files_mentioned_sc))
