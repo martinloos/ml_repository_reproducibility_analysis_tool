141ed4f0014c67e4c7e902b297bd4f8d5b9a3b7b
martinjl <martinjloos@mail.de>
2022-02-23 18:27:28 +0100

    updated result builder

--- result_builder.py
+++ result_builder.py
@@ -36,7 +36,7 @@ def build_result(repository_url, output_file):
 
     source_code_result = source_code_analysis.get_source_code_analysis()
     if not source_code_result:
-        source_code_result = build_zero_value_list(14)
+        source_code_result = build_zero_value_list(15)
 
     config_analysis = config_files_analysis.get_config_analysis_result()
     if not config_analysis:
@@ -53,10 +53,10 @@ def build_result(repository_url, output_file):
               source_code_result[0], source_code_result[1], source_code_result[2], source_code_result[3],
               source_code_result[4], source_code_result[5], source_code_result[6], source_code_result[7],
               source_code_result[8], source_code_result[9], source_code_result[10], source_code_result[11],
-              source_code_result[12], source_code_result[13], config_analysis[0], config_analysis[1],
-              config_analysis[2], config_analysis[3], config_analysis[4], config_analysis[5], config_analysis[6],
-              dataset_result[0], dataset_result[1], dataset_result[2], dataset_result[3], dataset_result[4],
-              binderhub_result[0]]
+              source_code_result[12], source_code_result[13], source_code_result[14], config_analysis[0],
+              config_analysis[1], config_analysis[2], config_analysis[3], config_analysis[4], config_analysis[5],
+              config_analysis[6], dataset_result[0], dataset_result[1], dataset_result[2], dataset_result[3],
+              dataset_result[4], binderhub_result[0]]
 
     analysis_result.extend(result)
 
@@ -79,8 +79,6 @@ def create_csv_file(output_file):
         csv_header = make_csv_header()
         write_list_to_csv(csv_header, output_file)
 
-    print('\nWrote result to ' + output_file + '\n')
-
 
 def make_csv_header():
     return ['repository_url', '# readmes', 'total readme lines', 'average readme lines', '# readme links',
@@ -89,8 +87,10 @@ def make_csv_header():
             '% of code lines', '# total comment lines', '% of comment lines', 'code-comment-ratio',
             'average pylint score', '# jupyter notebooks', '% jupyter notebooks /w header',
             '% jupyter notebooks /w footer', '# random seed declaration lines', '% fixed random seed lines',
-            'Data version control used?', '# hyperparameter documentation indicators', '# config files',
-            'OS specified in config file(s)', '# unique imports in config file(s)',
+            'Model serialization used?', '# hyperparameter logging indicators',
+            '% public available libraries in relation to all unique imports in source code (excl. local libs and '
+            'python standard libs)',
+            '# config files', 'OS specified in config file(s)', '# unique imports in config file(s)',
             '# unique imports in source code file(s)',
             '% strict unique imports in relation to all unique imports in config file(s)',
             '% specified unique (but not strict) imports in relation to all unique imports in config file(s)',
@@ -115,6 +115,7 @@ def check_repository(repository_url, output_file):
 
 
 def write_list_to_csv(result_list, output_file):
+    print('\nWrote result to ' + output_file + '\n')
     with open(output_file, 'a', newline='') as csv_file:
         wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         wr.writerow(result_list)
@@ -154,24 +155,26 @@ def build_terminal_response():
     table.add_row('% of jupyter notebooks /w footer', str(analysis_result[20]))
     table.add_row('# random seed lines', str(analysis_result[21]))
     table.add_row('% fixed random seed lines', str(analysis_result[22]))
-    table.add_row('Data version control used?', str(analysis_result[23]))
-    table.add_row('# hyperparameter documentation indicators', str(analysis_result[24]))
-    table.add_row('# config files', str(analysis_result[25]))
-    table.add_row('OS specified in config file(s)', str(analysis_result[26]))
-    table.add_row('# unique imports in config file(s)', str(analysis_result[27]))
-    table.add_row('# unique imports in source code file(s)', str(analysis_result[28]))
+    table.add_row('Model serializaton used?', str(analysis_result[23]))
+    table.add_row('# hyperparameter logging indicators', str(analysis_result[24]))
+    table.add_row('% public available libraries in relation to all unique imports in source code (excl. local libs '
+                  'and python standard libs)' + str(analysis_result[25]))
+    table.add_row('# config files', str(analysis_result[26]))
+    table.add_row('OS specified in config file(s)', str(analysis_result[27]))
+    table.add_row('# unique imports in config file(s)', str(analysis_result[28]))
+    table.add_row('# unique imports in source code file(s)', str(analysis_result[29]))
     table.add_row('% strict unique imports in relation to all unique imports in config file(s)',
-                  str(analysis_result[29]))
-    table.add_row('% specified unique (but not strict) imports in relation to all unique imports in config file(s)',
                   str(analysis_result[30]))
-    table.add_row('% unique imports used in source code + mentioned in config file(s) in relation to all used',
+    table.add_row('% specified unique (but not strict) imports in relation to all unique imports in config file(s)',
                   str(analysis_result[31]))
-    table.add_row('dataset folders found?', str(analysis_result[32]))
-    table.add_row('# dataset folder candidates', str(analysis_result[33]))
-    table.add_row('# dataset file candidates', str(analysis_result[34]))
-    table.add_row('# found dataset file candidates mentioned in source code', str(analysis_result[35]))
+    table.add_row('% unique imports used in source code + mentioned in config file(s) in relation to all used',
+                  str(analysis_result[32]))
+    table.add_row('dataset folders found?', str(analysis_result[33]))
+    table.add_row('# dataset folder candidates', str(analysis_result[34]))
+    table.add_row('# dataset file candidates', str(analysis_result[35]))
+    table.add_row('# found dataset file candidates mentioned in source code', str(analysis_result[36]))
     table.add_row('% mentioned dataset files in relation to all found candidate files',
-                  str(analysis_result[36]))
-    table.add_row('out-of-the-box buildable with binderhub?', str(analysis_result[37]))
+                  str(analysis_result[37]))
+    table.add_row('out-of-the-box buildable with binderhub?', str(analysis_result[38]))
     console.print(table)
     print('\n')
