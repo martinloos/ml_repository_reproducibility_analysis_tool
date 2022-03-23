c5b8ea8571fc6ff7d2bd433956b63c44cfe3ff20
martinjl <martinjloos@mail.de>
2022-03-23 02:54:25 +0100

    improved documentation and refactoring

--- modules/repository_structure.py
+++ modules/repository_structure.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 
-# TODO: doc
-
 import logging
 import os
 
 logger = logging.getLogger('log')
-
+# Stores all the directories which are present in the repository
 directory_list = []
+# Stores all the files which are present in the repository
 file_list = []
 
 
 def get_structure(root):
+    """
+        All directories found in the repository are stored into the directory_list. All files found are stored into
+        the file_list. Based on this, we build a list containing these lists, which will be returned.
+
+        Parameters:
+            root (str): The path to the root of the repository.
+
+        Returns:
+            repository_structure (List): A list containing the list of all directories, and the list of all files
+            present in the repository.
+    """
     for root, dirs, files in os.walk(root):
         for d in dirs:
             # directory_name, path_to_directory, directory_size
@@ -40,4 +50,7 @@ def get_structure(root):
     for element in sorted_flist:
         logger.debug(element)
 
-    return [sorted_dlist, sorted_flist]
+    repository_structure = [sorted_dlist, sorted_flist]
+
+    return repository_structure
+
