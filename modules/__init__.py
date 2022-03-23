ce1709588da647b3a6927ef665054795cedd9464
martinjl <martinjloos@mail.de>
2022-03-23 02:51:51 +0100

    update after refactoring

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,3 @@
-__all__ = ["repository_cloner", "repository_structure", "filter_repository_artefacts",
+__all__ = ["repository_cloner", "repository_structure", "filter_repository_artifacts",
            "readme_analysis", "license_analysis", "dataset_analysis", "python_source_code_analysis",
            "source_code_analysis", "config_files_analysis", "binderhub_call", "result_builder", "feedback_builder"]
-
