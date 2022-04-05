1d95fd9a52ba28908d799d2e5de5e51cbc222e50
martinjl <martinjloos@mail.de>
2022-04-05 04:22:11 +0200

    updated

--- modules/result_builder.py
+++ modules/result_builder.py
@@ -29,7 +29,7 @@ def build_result(repository_url, output_file):
     """
     readme_result = readme_analysis.get_readme_analysis()
     if not readme_result:
-        readme_result = build_zero_value_list(7)
+        readme_result = build_zero_value_list(8)
 
     license_result = license_analysis.get_license_result()
     if not license_result:
@@ -50,7 +50,7 @@ def build_result(repository_url, output_file):
     binderhub_result = binderhub_call.get_result()
 
     result = [repository_url, readme_result[0], readme_result[1], readme_result[2], readme_result[3], readme_result[4],
-              readme_result[5], readme_result[6], license_result[0], license_result[1], license_result[2],
+              readme_result[5], readme_result[6], readme_result[7], license_result[0], license_result[1], license_result[2],
               source_code_result[0], source_code_result[1], source_code_result[2], source_code_result[3],
               source_code_result[4], source_code_result[5], source_code_result[6], source_code_result[7],
               source_code_result[8], source_code_result[9], source_code_result[10], source_code_result[11],
@@ -84,10 +84,10 @@ def create_csv_file(output_file):
 
 def make_csv_header():
     return ['repository_url', '# readmes', 'total readme lines', 'average readme lines', '# readme links',
-            '# readme paper links', '# not accessible links', '% not accessible links', '# licences',
-            '# open-source licenses', '# non-open-source licenses', '# source code files', '# total lines of code',
-            '% of code lines', '# total comment lines', '% of comment lines', 'code-comment-ratio',
-            'average pylint score', '# jupyter notebooks', '% jupyter notebooks /w header',
+            '# readme paper links', '# not accessible links', '% not accessible links', 'readme dataset reference',
+            '# licences', '# open-source licenses', '# non-open-source licenses', '# source code files',
+            '# total lines of code', '% of code lines', '# total comment lines', '% of comment lines',
+            'code-comment-ratio', 'average pylint score', '# jupyter notebooks', '% jupyter notebooks /w header',
             '% jupyter notebooks /w footer', '# random seed declaration lines', '% fixed random seed lines',
             'Model serialization used?', '# hyperparameter logging indicators',
             '% public available libraries in relation to all unique imports in source code (excl. local libs and '
@@ -144,41 +144,45 @@ def build_terminal_response():
     table.add_row('# paper links', str(analysis_result[5]))
     table.add_row('# not accessible links', str(analysis_result[6]))
     table.add_row('% not accessible links', str(analysis_result[7]))
-    table.add_row('# licenses', str(analysis_result[8]))
-    table.add_row('# open-source licenses', str(analysis_result[9]))
-    table.add_row('# non-open-source licenses', str(analysis_result[10]))
-    table.add_row('# source code files', str(analysis_result[11]))
-    table.add_row('# total lines of code', str(analysis_result[12]))
-    table.add_row('% of code lines', str(analysis_result[13]))
-    table.add_row('# total comment lines', str(analysis_result[14]))
-    table.add_row('% of comment lines', str(analysis_result[15]))
-    table.add_row('code-comment-ratio', str(analysis_result[16]))
-    table.add_row('average pylint score (best is 10)', str(analysis_result[17]))
-    table.add_row('# jupyter notebooks', str(analysis_result[18]))
-    table.add_row('% of jupyter notebooks /w header', str(analysis_result[19]))
-    table.add_row('% of jupyter notebooks /w footer', str(analysis_result[20]))
-    table.add_row('# random seed lines', str(analysis_result[21]))
-    table.add_row('% fixed random seed lines', str(analysis_result[22]))
-    table.add_row('Model serializaton used?', str(analysis_result[23]))
-    table.add_row('# hyperparameter logging indicators', str(analysis_result[24]))
+    if analysis_result[8] == 0:
+        table.add_row('readme dataset reference', 'No')
+    else:
+        table.add_row('readme dataset reference', 'Yes')
+    table.add_row('# licenses', str(analysis_result[9]))
+    table.add_row('# open-source licenses', str(analysis_result[10]))
+    table.add_row('# non-open-source licenses', str(analysis_result[11]))
+    table.add_row('# source code files', str(analysis_result[12]))
+    table.add_row('# total lines of code', str(analysis_result[13]))
+    table.add_row('% of code lines', str(analysis_result[14]))
+    table.add_row('# total comment lines', str(analysis_result[15]))
+    table.add_row('% of comment lines', str(analysis_result[16]))
+    table.add_row('code-comment-ratio', str(analysis_result[17]))
+    table.add_row('average pylint score (best is 10)', str(analysis_result[18]))
+    table.add_row('# jupyter notebooks', str(analysis_result[19]))
+    table.add_row('% of jupyter notebooks /w header', str(analysis_result[20]))
+    table.add_row('% of jupyter notebooks /w footer', str(analysis_result[21]))
+    table.add_row('# random seed lines', str(analysis_result[22]))
+    table.add_row('% fixed random seed lines', str(analysis_result[23]))
+    table.add_row('Model serializaton used?', str(analysis_result[24]))
+    table.add_row('# hyperparameter logging indicators', str(analysis_result[25]))
     table.add_row('% public available libraries in relation to all unique imports in source code (excl. local libs '
-                  'and python standard libs)', str(analysis_result[25]))
-    table.add_row('# config files', str(analysis_result[26]))
-    table.add_row('OS specified in config file(s)', str(analysis_result[27]))
-    table.add_row('# unique imports in config file(s)', str(analysis_result[28]))
-    table.add_row('# unique imports in source code file(s)', str(analysis_result[29]))
+                  'and python standard libs)', str(analysis_result[26]))
+    table.add_row('# config files', str(analysis_result[27]))
+    table.add_row('OS specified in config file(s)', str(analysis_result[28]))
+    table.add_row('# unique imports in config file(s)', str(analysis_result[29]))
+    table.add_row('# unique imports in source code file(s)', str(analysis_result[30]))
     table.add_row('% strict unique imports in relation to all unique imports in config file(s)',
-                  str(analysis_result[30]))
-    table.add_row('% specified unique (but not strict) imports in relation to all unique imports in config file(s)',
                   str(analysis_result[31]))
-    table.add_row('% unique imports used in source code + mentioned in config file(s) in relation to all used',
+    table.add_row('% specified unique (but not strict) imports in relation to all unique imports in config file(s)',
                   str(analysis_result[32]))
-    table.add_row('dataset folders found?', str(analysis_result[33]))
-    table.add_row('# dataset folder candidates', str(analysis_result[34]))
-    table.add_row('# dataset file candidates', str(analysis_result[35]))
-    table.add_row('# found dataset file candidates mentioned in source code', str(analysis_result[36]))
+    table.add_row('% unique imports used in source code + mentioned in config file(s) in relation to all used',
+                  str(analysis_result[33]))
+    table.add_row('dataset folders found?', str(analysis_result[34]))
+    table.add_row('# dataset folder candidates', str(analysis_result[35]))
+    table.add_row('# dataset file candidates', str(analysis_result[36]))
+    table.add_row('# found dataset file candidates mentioned in source code', str(analysis_result[37]))
     table.add_row('% mentioned dataset files in relation to all found candidate files',
-                  str(analysis_result[37]))
-    table.add_row('out-of-the-box buildable with binderhub?', str(analysis_result[38]))
+                  str(analysis_result[38]))
+    table.add_row('out-of-the-box buildable with binderhub?', str(analysis_result[39]))
     console.print(table)
     print('\n')
